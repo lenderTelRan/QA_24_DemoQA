6d6d4bfc415225960560b4086d2db6f10a328e3f
lenderTelRan <lender.vladimir@gmail.com>
2024-03-04 19:55:22 +0200

    first commit

---
+++ src/test/java/locators/CssLocators.java
@@ -0,0 +1,44 @@
+package locators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CssLocators {
+    WebDriver wd;
+    @Test
+    public void start() {
+        wd = new ChromeDriver();
+        wd.navigate().to("https://demoqa.com/text-box");
+
+        WebElement fullName = wd.findElement(By.cssSelector("input"));
+        //System.out.printf("-->" + fullName.getAttribute("placeholder"));
+        fullName.click();
+        fullName.clear();
+
+        WebElement elEmail = wd.findElement(By.cssSelector("input[type='email']")); //find by tag + attribute
+        //System.out.printf("-->" + elEmail.getAttribute("placeholder"));
+
+        WebElement elAddress = wd.findElement(By.cssSelector("#currentAddress")); //find by id
+        //System.out.printf("-->" + elAddress.getTagName);
+
+        wd.quit();
+    }
+    @Test
+    public void checkButtons() {
+        wd = new ChromeDriver();
+        wd.navigate().to("https://demoqa.com/buttons");
+
+        WebElement button1 = wd.findElement(By.cssSelector("#doubleClickBtn"));
+        WebElement button2 = wd.findElement(By.cssSelector("#rightClickBtn"));
+        WebElement button3 = wd.findElement(By.cssSelector("#MPfhZ"));
+
+    }
+
+    @Test
+    public void checkBox() {
+        
+    }
+}
